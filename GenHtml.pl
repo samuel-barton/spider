12d9c93407724706d4b496fe6d2dded683904308
Samuel Barton <samuel.barton@maine.edu>
2016-06-28 16:34:30 -0400

    Changed the functionality of the pasword method to simplify its use.

--- GenHtml.pl
+++ GenHtml.pl
@@ -15,62 +15,49 @@ use v5.14;
 #
 #==============================================================================
 
-# the name of the html file we are outputting to.
-my $output_path = "output.html";
+# paths to html files
+my $password_output_path = 'html/password.html';
+my $success_output_path = 'html/success.html';
+my $fail_output_path = 'html/fail.html';
 
-# &welcome();
-&password();
+my $password_input_path = 'html/template/password.html';
+my $success_input_path = 'html/template/success.html';
+my $fail_input_path = 'html/template/fail.html';
 
-#==============================================================================
-#
-# Function name: welcome
-#
-# Parameters: none
-#
-# returns: void
-#
-# Description: This page simply prints the welcome screen asking the user to
-#              swipe their card.
-#
-#==============================================================================
-sub welcome
-{
-    my $output_file = &openFile();
-
-    &begin($output_file);
-
-    say $output_file "<h1>Welcome!</h1>";
-    say $output_file "<h2><i>Please scan your tag.</i></h2>";
-
-    &end($output_file);
-    close($output_file);
-}
-
+&password("jane");
 
 #==============================================================================
 #
 # Function name: password
 #
-# parameters: none
+# parameters: username
 #
 # Returns: void
 #
-# Decription: this function prints out the header for the top of the html file.
+# Description: this function generates the custom password entry page for each
+#              individual user.
 #
 #==============================================================================
 sub password
 {
-    my $output_file = &openFile();
+    # get parameter.
+    my $username = $_[0];
 
-    &begin($output_file);
+    # open the input and output files
+    my $input_file = &read($password_input_path);
+    my $output_file = &write($password_output_path);
 
-    say $output_file "<form>";
-    say $output_file "password:<br>";
-    say $output_file "<input type=\"text\" name=\"password\"><br>";
-    say $output_file "</form>";
+    # loop through the input file, and write each line to the output file, 
+    # substituting the parameter for every instance of USER. 
+    for my $line (<$input_file>)
+    {
+	chomp($line);
+	$line =~ s/USER/$username/;
+        say $output_file $line;
+    }
 
-    &end($output_file);
-
+    # close both files.
+    close($input_file);
     close($output_file);
 }
 
@@ -114,18 +101,40 @@ sub end
 
 #==============================================================================
 #
-# Function name: openFile
+# Function name: write
 #
-# parameters: none
+# parameters: path
 #
-# Returns: file handle to output.html
+# Returns: file handle to the file specified by $path
 #
-# Decription: this function opens the output file for writing.
+# Decription: this function opens the file for writing.
 #
 #==============================================================================
-sub openFile
+sub write
 { 
-    open(my $fh, ">", $output_path) or die "Couldn't open file: $!";
+    my $path = $_[0];
+
+    open(my $fh, ">", $path) or die "Couldn't open file: $!";
+
+    return $fh;
+}
+
+#==============================================================================
+#
+# Function name: read
+#
+# parameters: path
+#
+# Returns: file handle to the file specified by $path
+#
+# Decription: this function opens the file for reading.
+#
+#==============================================================================
+sub read
+{ 
+    my $path = $_[0];
+
+    open(my $fh, "<", $path) or die "Couldn't open file: $!";
 
     return $fh;
 }
