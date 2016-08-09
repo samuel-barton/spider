1012e7a5a40203cfd261cb8b0e94311e9e16fb38
Samuel Barton <samuel.barton@maine.edu>
2016-08-09 13:13:52 -0400

    Added configuration for setting up Spider.pl as a service.

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-#==============================================================================
+#------------------------------------------------------------------------------
 #
 # File name: config.sh
 #
@@ -12,7 +12,7 @@
 #
 # Description: This script will handle deploying project spider on some system.
 #
-#==============================================================================
+#------------------------------------------------------------------------------
 
 # install dependencies
 sudo apt-get update;
@@ -30,3 +30,21 @@ cat apache/000-default.conf | sed "s:/home/[a-zA-Z0-9]+/:/home/$(USER)/";
 # put the apache config file in the correct directory
 sudo cp apache/000-default.conf /etc/apache2/sites-enabled/;
 
+# do the configuration for making Spider.pl a service
+
+# create a user for the spider process
+sudo useradd --create-home --system -G input spider;
+# put the service file in the upstart service directory
+sudo cp spider.conf /etc/init;
+
+# ask the user if they would like to reboot the machine, which will apply the
+# changes made thus far.
+echo "To finish the configuration, the machine must restart."
+echo -n "Do you want to restart now [y/n]: "
+# get the users decision
+decision=${read};
+
+if [ $decision == "y" || $decision == "" ]
+then
+    sudo reboot
+fi
